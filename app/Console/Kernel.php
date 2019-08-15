7118666feaecef28378c29c5ac976cde3833983c
Munteanu Petrisor <munteanu_petrisor2710@yahoo.com>
2019-08-15 12:07:23 +0300

    Add telescope for debugging

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,6 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
+        $schedule->command('telescope:prune --hours=48')->daily();
         // $schedule->command('inspire')
         //          ->hourly();
     }
